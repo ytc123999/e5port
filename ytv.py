7e36c8ee4266bfa03c27b7b6eb6222b480ddaafc
ytc123999 <81024823+ytc123999@users.noreply.github.com>
2023-07-21 10:34:59 +0800

    Create ytv.py

---
+++ ytv.py
@@ -0,0 +1,15 @@
+import time
+
+def focus_timer(minutes):
+    seconds = minutes * 60
+    while seconds:
+        mins, secs = divmod(seconds, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        seconds -= 1
+    print("Time's up!")
+
+if __name__ == "__main__":
+    minutes = int(input("Enter the number of minutes to focus: "))
+    focus_timer(minutes)
